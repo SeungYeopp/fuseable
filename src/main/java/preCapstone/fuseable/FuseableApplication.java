ce877defbb33391d700165c1379ea00ad8ec3a1c
SeungYeopp <syt05342@gmail.com>
2022-09-12 12:36:05 +0900

    Initial commit

---
+++ src/main/java/preCapstone/fuseable/FuseableApplication.java
@@ -0,0 +1,13 @@
+package preCapstone.fuseable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuseableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuseableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/preCapstone/fuseable/FuseableApplicationTests.java
@@ -0,0 +1,13 @@
+package preCapstone.fuseable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuseableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
